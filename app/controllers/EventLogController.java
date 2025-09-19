98d2ea475f1ea0dff24717736e5a96021920d1df
leilaomerhodziczulic <leila.omerhodzic.work@gmail.com>
2025-09-20 00:17:01 +0200

    Added EventLogController and routes in routes file

--- app/controllers/EventLogController.java
+++ app/controllers/EventLogController.java
@@ -1,4 +1,33 @@
 package controllers;
 
-public class EventLogController {
+import io.ebean.DB;
+import models.EventLog;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class EventLogController extends Controller {
+    public Result login(){
+        EventLog eventLog = new EventLog("login");
+        DB.save(eventLog);
+
+        return ok("Login action up!");
+
+    }
+
+    public Result purchase(){
+        EventLog eventLog = new EventLog("purchase");
+        DB.save(eventLog);
+
+        return ok("Purchase action up!");
+
+    }
+
+    public Result search(){
+        EventLog eventLog = new EventLog("search");
+        DB.save(eventLog);
+
+        return ok("Search action up!");
+    }
+
+
 }
--- app/models/EventLog.java
+++ app/models/EventLog.java
@@ -9,8 +9,14 @@ import jakarta.persistence.*;
 @Entity
 public class EventLog extends Model{
     @Id
-
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
+
     public String eventType;
     public Timestamp timestamp;
+
+    public EventLog(String eventType){
+        this.timestamp = new Timestamp(System.currentTimeMillis());
+        this.eventType = eventType;
+    }
 }
